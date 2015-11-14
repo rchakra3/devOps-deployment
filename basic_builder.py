18283e631bd873322a872d78551600e110ec9532
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-11-14 16:58:28 -0500

    Branch specific container

--- basic_builder.py
+++ basic_builder.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def initiate_build():
-    cmd = ["sh","./create_img.sh"]
-    p = subprocess.check_call(cmd, stdout = subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            stdin=subprocess.PIPE)
-    print "Fine"
-    return '200'
+	branch_name = "master"
+	if request.args.get('branch') is not None:
+		branch_name = request.args['branch']
+	cmd = ["sh","./create_img.sh", branch_name]
+	p = subprocess.check_call(cmd, stdout = subprocess.PIPE,
+							stderr=subprocess.PIPE,
+							stdin=subprocess.PIPE)
+	print "Fine"
+	return '200'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=42000)
--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/rchakra3/simple-flask-app.git
+git clone -b $1 https://github.com/rchakra3/simple-flask-app.git
 cd simple-flask-app
 pip install -r requirements.txt
 python src/app.py
