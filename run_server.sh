686b85674c6bb4b4c8e919bce4037007cef72c98
Rohan Chakravarthy <rohanc219@gmail.com>
2015-12-05 23:26:28 -0500

    Update num workers to 1

--- run_server.sh
+++ run_server.sh
@@ -3,4 +3,4 @@
 git clone -b $1 https://github.com/rchakra3/simple-flask-app.git
 cd simple-flask-app
 pip install -r requirements.txt
-gunicorn -w 4 -b 127.0.0.1:5000 src.app:app
+gunicorn -w 1 -b 127.0.0.1:5000 src.app:app
